ac2296381a61b781ba9aea2e8ad0cbd07170fcd6
= <=>
2023-01-17 21:44:38 +0300

    SQL Udacity

---
+++ aggregation/DISTINCT.sql
@@ -0,0 +1,21 @@
+DISTINCT --is always used in SELECT statements, and it provides the unique rows for all columns written in the SELECT statement. 
+-- Therefore, you only use DISTINCT once in any particular SELECT statement.
+SELECT DISTINCT column1, column2, column3
+FROM table1;
+
+You would not write:
+
+SELECT DISTINCT column1, DISTINCT column2, DISTINCT column3
+FROM table1;
+
+-- You can think of DISTINCT the same way you might think of the statement "unique".
+
+-- DISTINCT - Expert Tip
+-- It’s worth noting that using DISTINCT, particularly in aggregations, can slow your queries down quite a bit.
+
+SELECT account_id,
+       channel,
+       COUNT(id) as events
+FROM web_events
+GROUP BY account_id, channel
+ORDER BY account_id, channel DESC
--- aggregation/group_by.sql
+++ aggregation/group_by.sql
@@ -29,3 +29,41 @@ FROM orders o
 JOIN accounts a
 ON a.id = o.account_id
 GROUP BY a.name;
+
+-- Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
+
+SELECT w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id 
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+-- Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+
+SELECT w.channel, COUNT(*)
+FROM web_events w
+GROUP BY w.channel
+
+-- Who was the primary contact associated with the earliest web_event?
+
+SELECT a.primary_poc
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+-- 6 What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+
+SELECT a.name, MIN(total_amt_usd) smallest_order
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY smallest_order;
+
+-- Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. 
+-- Order from the fewest reps to most reps.
+
+
