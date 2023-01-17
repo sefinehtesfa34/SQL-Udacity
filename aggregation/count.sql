1bbdc129793f72dd0c5f095cc4bd479893ac0b8c
= <=>
2023-01-17 14:52:56 +0300

    SQL Udacity

--- aggregation/count.sql
+++ aggregation/count.sql
@@ -5,3 +5,9 @@ FROM accounts; -- Will return one number which is the count of the numebr of row
 SELECT COUNT(accounts.id)
 FROM accounts;
 -- These two statements are equivalent, but this isn't always the case, which we will see in the next video.
+
+
+SELECT COUNT(*) AS order_count
+FROM orders
+WHERE occurred_at >= '2016-12-01'
+AND occurred_at < '2017-01-01'
