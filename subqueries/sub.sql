6772a44bb34ec9341ad4bfe02d8dad60188a4003
= <=>
2023-01-22 18:47:34 +0300

    subqueries

--- subqueries/sub.sql
+++ subqueries/sub.sql
@@ -4,3 +4,15 @@ SELECT product_id,
 FROM db.product
 Where price > (SELECT AVG(price)
               FROM db.product)
+
+
+
+SELECT AVG(standard_qty) avg_std, AVG(gloss_qty) avg_gls, AVG(poster_qty) avg_pst
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+     (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+
+SELECT SUM(total_amt_usd)
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+      (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
