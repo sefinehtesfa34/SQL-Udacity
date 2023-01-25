5679d168a91be9ed0aa733dd03756a4b67f9cfc5
= <=>
2023-01-25 12:57:53 +0300

    Window functions

--- window_functions/partition.sql
+++ window_functions/partition.sql
@@ -2,3 +2,55 @@
 SELECT standard_amt_usd,
        SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
 FROM orders
+
+SELECT standard_amt_usd,
+       DATE_TRUNC('year', occurred_at) as year,
+       SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
+FROM orders
+
+To emphasize the similarities and differences between group by/aggregation queries and window functions, let’s review the details below.
+
+Similarities
+Both groups by/aggregation queries and window functions serve the same use case. Synthesizing information over time and often grouped by a column (e.g., a region, month, customer group, etc.)
+
+Differences
+The difference between group by/aggregation queries and window functions is simple. The output of window functions retains all individual records whereas the group by/aggregation queries condense or collapse information.
+
+Key Notes
+You can’t use window functions and standard aggregations in the same query. More specifically, you can’t include window functions in a GROUP BY clause.
+Feel free to use as many window functions as you’d like in a single query. E.g., if you’d like to have an average, sum, and count aggregate function that captures three metrics’ running totals, go for it.
+Code from the Video*
+
+SELECT order_id,
+       order_total,
+       order_price,
+       SUM(order_total) OVER
+           (PARTITION BY month(order_date) ORDER BY order_date) AS running_monthly_sales,
+       COUNT(order_id) OVER
+           (PARTITION BY month(order_date) ORDER BY order_date) AS running_monthly orders,
+       AVG(order_price) OVER
+           (PARTITION BY month(order_date) ORDER BY order_date) AS average_monthly_price
+FROM  amazon_sales_db
+WHERE order_date < '2017-01-01';
+*Note - This code will not run in the Workspaces as written.
+
+
+
+-- Now that you understand how window functions work, let's 
+-- practice applying the same aggregates that you would under normal circumstances.
+
+-- Code from the Video
+
+SELECT id,
+       account_id,
+       standard_qty,
+       DATE_TRUNC('month', occurred_at) AS month,
+       DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS dense_rank,
+       SUM(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS sum_standard_qty,
+       COUNT(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS count_standard_qty,
+       AVG(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS avg_standard_qty,
+       MIN(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS min_standard_qty,
+       MAX(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month', occurred_at)) AS max_standard_qty
+FROM orders
+
+
